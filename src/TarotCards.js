85edea717ce4830e5bc3a65e4bc164fa29f32a5a
caseymorris321 <morrisacasey@gmail.com>
2023-06-07 15:10:47 -0400

    Fixed bugs and updated appearance

---
+++ src/TarotCards.js
@@ -0,0 +1,23 @@
+import TheMagician from './images/TheMagician.png'; // Replace tarotCard.jpg with your actual image file name and path
+import TheFool from './images/TheFool.png'; // Replace tarotCard.jpg with your actual image file name and path
+
+
+
+const tarotCards = [
+    {
+        name: 'The Fool',
+        description: 'The Fool represents new beginnings, spontaneity, and innocence. It signifies stepping into the unknown with an open mind and embracing life\'s journey.',
+        keywords: 'New beginnings, Spontaneity, Innocence',
+        image: TheFool
+    },
+    {
+        name: 'The Magician',
+        description: 'The Magician symbolizes manifestation, power, and creative energy. It suggests harnessing your skills and resources to create the reality you desire.',
+        keywords: 'Manifestation, Power, Creative energy',
+        image: TheMagician
+    },
+    // Add more card objects with their respective information
+];
+
+
+export default tarotCards;
