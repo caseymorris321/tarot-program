1b28534ac83d174a8f04162a65d88c49c0691309
caseymorris321 <morrisacasey@gmail.com>
2023-06-08 10:12:16 -0400

    added more cards

--- src/TarotCards.js
+++ src/TarotCards.js
@@ -1,5 +1,19 @@
 import TheMagician from './images/TheMagician.png'; // Replace tarotCard.jpg with your actual image file name and path
 import TheFool from './images/TheFool.png'; // Replace tarotCard.jpg with your actual image file name and path
+import TheHighPriestess from './images/TheHighPriestess.png';
+import TheEmpress from './images/TheEmpress.png';
+import TheEmperor from './images/TheEmperor.png';
+import TheHierophant from './images/TheHierophant.png';
+import TheLovers from './images/TheLovers.png';
+import TheChariot from './images/TheChariot.png';
+// import Strength from './images/Strength.png';
+import TheHermit from './images/TheHermit.png';
+import TheWheelOfFortune from './images/TheWheelOfFortune.png'
+import Justice from './images/Justice.png'
+
+
+
+
 
 
 
@@ -16,7 +30,70 @@ const tarotCards = [
         keywords: 'Manifestation, Power, Creative energy',
         image: TheMagician
     },
-    // Add more card objects with their respective information
+    {
+        name: 'The High Priestess',
+        description: 'The High Priestess represents a gateway to the subconscious and the spiritual realms, inviting you to explore your inner world and trust your intuition.',
+        keywords: 'Intuition, Mystery, Feminine Energy',
+        image: TheHighPriestess
+    },
+    {
+        name: 'The Empress',
+        description: 'The Empress symbolizes fertility, abundance, nurturing, and the creative force.',
+        keywords: 'Fertility, Nourishment, Connection with Nature and the Earth',
+        image: TheEmpress
+    },
+    {
+        name: 'The Emperor',
+        description: 'The Emperor symbolizes authority, structure, and stability, embodying the qualities of leadership and the ability to create a solid foundation.',
+        keywords: 'Authority, Structure, Leadership',
+        image: TheEmperor
+    },
+    {
+        name: 'The Hierophant',
+        description: 'The Hierophant symbolizes tradition, spirituality, and the pursuit of higher knowledge, representing a connection to established belief systems and the guidance of spiritual teachers.',
+        keywords: 'Tradition, Spirituality, Guidance',
+        image: TheHierophant
+    },
+    {
+        name: 'The Lovers',
+        description: 'The Lovers symbolizes deep connections, love, harmony, and the choices we make in relationships, representing the union of opposites and the power of choice in matters of the heart.',
+        keywords: 'Love, Harmony, Choice',
+        image: TheLovers
+    },
+    {
+        name: 'The Chariot',
+        description: 'The Chariot symbolizes determination, willpower, and the ability to overcome challenges, representing the victorious pursuit of goals through focused action and self-control.',
+        keywords: 'Determination, Willpower, Victory',
+        image: TheChariot
+    },
+    {
+        name: 'Strength',
+        description: 'Strength symbolizes inner courage, resilience, and the power to overcome challenges by harnessing one\'s inner strength and gentle persuasion rather than brute force.',
+        keywords: 'Innter Strength, Patience, Gentle Power',
+        image: ''
+    },
+    {
+        name: 'The Hermit',
+        description: 'The Hermit symbolizes introspection, solitude, and inner guidance.',
+        keywords: 'Isolation, introspection, wisdom',
+        image: TheHermit
+    },
+    {
+        name: 'The Wheel of Fortune',
+        description: 'The Wheel of Fortune represents the ever-changing cycles of life and destiny.',
+        keywords: 'Destiny, Change, Luck',
+        image: TheWheelOfFortune
+    },
+    {
+        name: 'Justice',
+        description: 'Justice represents truth, fairness, and the consequences of one\'s actions.',
+        keywords: 'Balance, Fairness, Impartiality',
+        image: Justice
+    },
+
+
+
+
 ];
 
 
