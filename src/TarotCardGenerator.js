478503d2ad1b1c2bec2174957833fb677e57ff43
caseymorris321 <morrisacasey@gmail.com>
2023-07-05 17:16:21 -0400

    preloader

--- src/TarotCardGenerator.js
+++ src/TarotCardGenerator.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect  } from 'react';
 import tarotCards from './TarotCards';
 import tarotDeckCover from './images/TarotDeckCover.png';
 import loadingImage from './images/loading.gif';
