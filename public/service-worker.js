b8fb9b427a52e1d61ca076716af3729892d5a7a4
caseymorris321 <morrisacasey@gmail.com>
2023-07-06 00:01:01 -0400

    service worker?

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,3 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
-
 const CACHE_NAME = 'tarot-card-generator-cache-v1';
 const urlsToCache = [
   '/',
@@ -13,28 +11,36 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  const preCache = async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(urlsToCache);
-  };
-
-  event.waitUntil(preCache());
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-const networkFirstStrategy = new workbox.strategies.NetworkFirst();
-workbox.routing.registerRoute('/', networkFirstStrategy);
-workbox.routing.registerRoute('/index.html', networkFirstStrategy);
-workbox.routing.registerRoute('/TarotCardGenerator.css', networkFirstStrategy);
-workbox.routing.registerRoute('/TarotCardGenerator.js', networkFirstStrategy);
-workbox.routing.registerRoute('/images/TarotDeckCover.png', networkFirstStrategy);
-workbox.routing.registerRoute('/images/loading.png', networkFirstStrategy);
-workbox.routing.registerRoute('/images/loading-multiple.png', networkFirstStrategy);
-// Add routes for other images used in your app
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(networkFirstStrategy.handle({ event }));
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // If the response is found in the cache, return it
+        if (response) {
+          return response;
+        }
+
+        // If the response is not found in the cache, fetch it from the network
+        return fetch(event.request)
+          .then((networkResponse) => {
+            // If the network response is valid, add it to the cache and return it
+            if (networkResponse && networkResponse.status === 200) {
+              const clonedResponse = networkResponse.clone();
+              caches.open(CACHE_NAME)
+                .then((cache) => cache.put(event.request, clonedResponse));
+            }
+
+            return networkResponse;
+          })
+          .catch((error) => {
+            console.error('Error fetching resource:', error);
+          });
+      })
+  );
 });
