36bdbf46a4f59f0e6c9a8aca7066c05373090f4a
caseymorris321 <morrisacasey@gmail.com>
2023-07-06 08:11:51 -0400

    test service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = 'my-cache';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/main.js',
-  '/static/css/main.css',
-  // Add any other assets you want to cache
-];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open('your-cache-name').then((cache) => {
+      return cache.addAll([
+        // Add the URLs of the assets you want to cache
+        '/',
+        '/index.html',
+        '/manifest.json',
+        // Add other assets like CSS, JavaScript files, and images
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
