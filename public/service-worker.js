76495625335d50e008cb7d4ef65e4470677b90bf
caseymorris321 <morrisacasey@gmail.com>
2023-07-06 08:22:11 -0400

    added assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const imagesDirectory = path.join(__dirname, 'src', 'images');
+const cacheEntries = [];
+
+// Read the images directory
+fs.readdirSync(imagesDirectory).forEach((file) => {
+  // Get the file extension
+  const extension = path.extname(file);
+
+  // Check if it's an image file
+  if (extension === '.png' || extension === '.jpg' || extension === '.jpeg') {
+    // Generate the URL and add it to the cache entries
+    const imageUrl = `/images/${file}`;
+    cacheEntries.push(imageUrl);
+  }
+});
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('your-cache-name').then((cache) => {
@@ -6,7 +25,12 @@ self.addEventListener('install', (event) => {
         '/',
         '/index.html',
         '/manifest.json',
-        // Add other assets like CSS, JavaScript files, and images
+        '/src/TarotCardGenerator.css',
+        '/src/App.js',
+        'src/TarotCardGenerator.js',
+        'src/index.js',
+        'src/TarotCards.js',
+        ...cacheEntries,
       ]);
     })
   );
