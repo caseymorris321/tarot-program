3b9b2bd4498ee956becc8a83c1648a3251418957
caseymorris321 <morrisacasey@gmail.com>
2023-07-05 23:23:13 -0400

    removed service worker

--- src/index.js
+++ src/index.js
@@ -11,16 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
