28412fc3ef561d58ff20aa3e4e444b2e92ecc2d1
caseymorris321 <morrisacasey@gmail.com>
2023-07-05 22:58:49 -0400

    offline capability

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'tarot-card-generator-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/TarotCardGenerator.css',
+  '/TarotCardGenerator.js',
+  '/images/TarotDeckCover.png',
+  '/images/loading.png',
+  '/images/loading-multiple.png',
+  ...getImageUrls('src/images/')
+];
+
+function getImageUrls(directory) {
+    const context = require.context(directory, true, /\.(png|jpg|jpeg|gif|svg)$/);
+    return context.keys().map(context);
+  }
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        // Return the cached response if available, or fetch from network
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
--- src/TarotCardGenerator.js
+++ src/TarotCardGenerator.js
@@ -20,6 +20,19 @@ function TarotCardGenerator() {
     image.src = tarotDeckCover;
   }, []);
 
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered:', registration);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
+
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
