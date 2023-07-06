ebfcd8e3e7423f68de869dd2864ca995974eb5be
caseymorris321 <morrisacasey@gmail.com>
2023-07-05 23:21:21 -0400

    createroot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
