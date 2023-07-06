9ac66201b99624999189f64811c3ac63ab255ab9
caseymorris321 <morrisacasey@gmail.com>
2023-07-06 08:59:27 -0400

    .

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,35 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+
+workbox.setConfig({ debug: false });
+
 self.addEventListener('install', (event) => {
+  const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/favicon.ico',
+    // Add other static assets from the public directory
+    '/logo192.png',
+    '/logo512.png'
+  ];
+
+  const assetsToPrecache = [
+    // Add other assets from the src directory
+    '/static/js/main.chunk.js',
+    '/static/js/0.chunk.js',
+    '/static/js/bundle.js',
+    '/static/css/main.chunk.css'
+  ];
+
   event.waitUntil(
     caches.open('your-cache-name').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        // Add other assets like CSS, JavaScript files, and images
-      ]);
+      return cache.addAll(urlsToCache);
+    })
+  );
+
+  event.waitUntil(
+    caches.open('your-cache-name').then((cache) => {
+      return cache.addAll(assetsToPrecache);
     })
   );
 });
