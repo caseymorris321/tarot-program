9ff18e48e28b60a3c9533f0f2e47d3450a2bec6b
caseymorris321 <morrisacasey@gmail.com>
2023-07-05 23:29:11 -0400

    service worker?

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'tarot-card-generator-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/TarotCardGenerator.css',
+  '/TarotCardGenerator.js',
+  '/images/TarotDeckCover.png',
+  '/images/loading.png',
+  '/images/loading-multiple.png',
+  // Add paths to other images used in your app
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return the cached response if available, or fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
