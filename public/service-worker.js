5b155cb5fc980e209f79fafa21f253e6f4a6bc7e
caseymorris321 <morrisacasey@gmail.com>
2023-07-06 09:19:00 -0400

    .

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('your-cache-name').then((cache) => {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/manifest.json',
+        'src/TarotCardGenerator.css'
         // Add other assets like CSS, JavaScript files, and images
       ]);
     })
